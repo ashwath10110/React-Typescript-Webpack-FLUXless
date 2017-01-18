48933eba471e663ef6292847d06f0163a4e08b5e
Ashwath Bharadwaj <gmail@gmail.com>
2017-01-18 14:23:15 +0530

    init react webpack

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import 'bootswatch/paper/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './main.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router, Route, Redirect, IndexRoute } from 'react-router';
+
+
+export default class App extends React.Component<{}, {}> {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello World!!</h1>
+			</div>
+		);
+	}
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
